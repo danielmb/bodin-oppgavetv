f84e1c8b884fed96a557b8e7fd40cb1b1f6f0cb2
danielmb <danielmbreivik@hotmail.com>
2022-05-09 15:16:37 +0200

    Database, api wip

--- pages/api/task/index.js
+++ pages/api/task/index.js
@@ -1,72 +1,43 @@
-import excuteQuery from '../../../lib/db';
-export default async function handler(req, res) {
-  switch (req.method) {
-    case 'GET':
-      {
-        try {
-          let result;
-          result = await excuteQuery({
-            query: `SELECT * FROM tasks`,
-            values: [],
-          });
-          if (result.error)
-            return res.status(500).json({ error: result.error });
+import nextConnect from 'next-connect';
+import { connectToDatabase } from '../../../lib/db';
+const apiRoute = nextConnect({
+  onError(err, req, res) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  },
+  onNoMatch(req, res) {
+    console.log('no match');
+    res.status(404).json({ error: 'Not found' });
+  },
+});
 
-          res.status(200).json(result);
-        } catch (error) {
-          console.log(error);
-          res.status(500).json({ error });
-        }
-      }
-      break;
-    case 'POST':
-      {
-        try {
-          const body = req.body;
-          const {
-            what,
-            where,
-            priority,
-            start,
-            doneby,
-            done,
-            created_at,
-            updated_at,
-          } = body;
-          let dateObjects = [
-            'start',
-            'doneby',
-            'done',
-            'created_at',
-            'updated_at',
-          ];
-          for (let d of dateObjects) {
-            if (body[d]) {
-              body[d] = moment(body[d]).format('YYYY-MM-DD HH:mm:ss');
-            }
-          }
-          const result = await excuteQuery({
-            query: `INSERT INTO tasks (what, where, priority, start, doneby, done, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-            values: [
-              what,
-              where,
-              priority,
-              start,
-              doneby,
-              done,
-              created_at,
-              updated_at,
-            ],
-          });
-          if (result.error)
-            return res.status(500).json({ error: result.error });
-          res.status(200).json(result);
-        } catch (error) {
-          res.status(500).json({ error });
-        }
-      }
-      break;
-    default:
-      res.status(405).json({ error: 'Method not allowed' });
-  }
-}
+apiRoute.get(async () => {
+  console.log('!');
+  let { db } = await connectToDatabase();
+
+  let results = await db.collection('tasks').find({}).toArray();
+  return results;
+});
+apiRoute.post(async (req, res) => {
+  let { db } = await connectToDatabase();
+  console.log('!');
+  let body = req.body;
+  if (typeof body !== 'object')
+    return res.status(400).json({ error: 'invalid body' });
+  if (body.constructor !== Object)
+    return res.status(400).json({ error: 'invalid body' });
+
+  let dbData = {
+    ...body,
+    ...{
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+  };
+  let result = await db.collection('tasks').insertOne(dbData);
+  if (!result.acknowledged)
+    return res.status(500).json({ error: 'insert failed' });
+  console.log(result);
+  return res.status(200).json({ ...{ _id: result.insertedId }, ...dbData });
+});
+export default apiRoute;
